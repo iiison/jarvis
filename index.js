0ff7b59d02171779f16c4817be5affd0354559a0
bharatrm <bharat.soni@redmart.com>
2018-02-06 14:38:08 +0530

    FEAT: working flow

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ if (command && command.length > 0) {
   let execute = `${command}${_.capitalize(argv.type)}`
 
   if ( confs[execute].constructor === Function ) {
-    confs[execute](argv)
+    confs[execute]()
   } else {
     console.log(chalk.red(`\n \n Command not found!`))
   }
